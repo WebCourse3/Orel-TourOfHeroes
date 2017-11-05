a10acb316ada1145bc47eed52c7ccf55c79796ad
OrelZluf <orelz16@gmail.com>
2017-11-05 16:50:53 +0200

    started Testing

---
+++ test/test.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+
+//var router = require('../router');
+
+//var originHeroesList = router.heroesList;
+
+
+// describe('GET all heroes', function() {
+// 	describe('#indexOf()', function() {
+// 		it('count of heroes equal', function() {
+// 			//assert.equal(router.heroesList, [1,2,3].indexOf(4));
+// 			console.log(router);
+// 		});
+// 	});
+// });
+//
+
+// describe('Array', function() {
+// 	describe('#indexOf()', function() {
+// 		it('should return -1 when the value is not present', function() {
+// 			assert.equal(-1, [1,2,3].indexOf(4));
+// 		});
+// 	});
+// });
