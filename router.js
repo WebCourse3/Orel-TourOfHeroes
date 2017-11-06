2176aaa41331a2e22f5184536c3b97fee4cfe74a
OrelZluf <orelz16@gmail.com>
2017-11-06 15:27:34 +0200

    Finished the testing with mock and sinon

--- router.js
+++ router.js
@@ -8,76 +8,50 @@ router.use(bodyParser.json());
 
 module.exports = router;
 
-// var heroesList = [];
-
-// {
-//
-// 	fs.readFile('./heroes.json', 'utf8', function readFileCallback(err, data){
-// 		if (err){
-// 			console.log(err);
-// 		}
-// 		else {
-// 			heroesList = JSON.parse(data)["heroesList"];
-// 		}});
-// }
+var heroHandler = require('./heroHandler');
 
 // Get all heroes
 router.get('/heroes', function (req, res) {
+
+	var heroesList = heroHandler.getAllHeroes();
+
 	res.send(JSON.stringify(heroesList));
 });
 
 // Get hero by id
 router.get('/heroes/:id', function (req, res) {
 
-	var heroID = req.params.id;
+	var currHero = heroHandler.getHeroByID(req.params.id);
 
-	heroesList.forEach(function(currHero){
-		if (currHero.id == heroID){
-			res.send(JSON.stringify(currHero));
-		}
-	});
+	res.send(JSON.stringify(currHero));
 });
 
 // Update hero by ID - put
 router.put('/heroes/:id', function (req, res) {
 
-	var heroID = req.params.id;
-	var userExist = false;
+	var updatedStatus = heroHandler.updateHeroByID(req.params.id, req.params.name);
 
-	heroesList.forEach(function(currHero){
-		if (currHero.id == heroID){
-			currHero.name = req.query.name;
-			userExist = true;
-			res.send('updated');
-		}
-	});
-
-	if (!userExist){
-		res.send('user not exist');
+	if (updatedStatus){
+		res.send('Hero Updated');
+	}
+	else{
+		res.send('Hero not found');
 	}
 });
 
 // Update hero by ID - post
 router.post('/heroes', function(req, res){
 
-	var rquestDetails = JSON.stringify(req.body);
-
 	var heroID = req.body.id;
 	var heroName = req.body.name;
-	var userExist = false;
 
-	heroesList.forEach(function(currHero){
-		if (currHero.id == heroID){
-			userExist = true;
-			currHero.name = heroName;
-		}
-	});
+	var updatedStatus = heroHandler.updateHeroByID(heroID, heroName);
 
 	if (!userExist){
-		heroesList.push({"id" : heroID, "name" : heroName});
+		addHero(heroID, heroName);
 	}
 
-	res.send(JSON.stringify(heroesList));
+	res.send(JSON.stringify(heroHandler.getAllHeroes()));
 });
 
 // Delete hero by id
@@ -85,87 +59,27 @@ router.delete('/heroes/:id', function (req, res){
 
 	var heroID = req.params.id;
 
-	heroesList.forEach(function(currHero, index){
+	var deletedStatus = heroHandler.deleteHeroByID(heroID);
 
-		if (currHero.id == heroID){
-
-			heroesList.splice(index, 1);
-			res.send('Hero deleted');
-		}
-	});
-
-	res.send('Hero Not Found');
+	if (deletedStatus){
+		res.send('Hero deleted');
+	}
+	else {
+		res.send('Hero Not Found');
+	}
 });
 
 // Delete hero by term
-router.delete('/heroes', function (req, res){
+router.delete('/heroes', function (req, res) {
 
-	var term = req.query.name;
-	var herosToDelete = [];
+	var heroTerm = req.query.name;
 
-	heroesList.forEach(function(currHero, index){
+	var deletedStatus = heroHandler.deleteHeroByTerm(heroTerm);
 
-		if (currHero.name.includes(term)){
-			herosToDelete.push(index);
-		}
-	});
-
-	for (var index = herosToDelete.length - 1; index >= 0; index--){
-		heroesList.splice(herosToDelete[index], 1);
-	}
-
-	if (herosToDelete.length == 0){
-		res.send('No heroes with this term');
-	}
-	else {
+	if (deletedStatus) {
 		res.send('Heroes deleted');
 	}
-});
-
-
-module.exports.heroesList = heroesList;
-
-var heroesList =  [
-	{
-		"id": "1",
-		"name": "Moshe"
-	},
-	{
-		"id": "2",
-		"name": "Juliet"
-	},
-	{
-		"id": "3",
-		"name": "Gilberto"
-	},
-	{
-		"id": "4",
-		"name": "Fanny"
-	},
-	{
-		"id": "5",
-		"name": "Elizbeth"
-	},
-	{
-		"id": "6",
-		"name": "Brendon"
-	},
-	{
-		"id": "7",
-		"name": "Victor"
-	},
-	{
-		"id": "8",
-		"name": "Messi"
-	},
-	{
-		"id": "9",
-		"name": "Hillary"
-	},
-	{
-		"id": "10",
-		"name": "Tamera"
+	else {
+		res.send('No heroes with this term');
 	}
-];
-
-
+});
--- test/test.js
+++ test/test.js
@@ -1,24 +1,107 @@
 var assert = require('assert');
+var mocha = require('mocha');
+var sinon = require('sinon');
 
-//var router = require('../router');
+var heroHandler = require('../heroHandler');
 
-//var originHeroesList = router.heroesList;
+// Get functions test
+describe('heroHandler functions', function() {
+	describe('GET functions', function () {
+		describe('Get all heroes', function () {
+			it('get all heroes', function () {
 
+				var heroesList = heroHandler.getAllHeroes();
+				assert.equal(heroesList.length, 10);
+			});
+		});
 
-// describe('GET all heroes', function() {
-// 	describe('#indexOf()', function() {
-// 		it('count of heroes equal', function() {
-// 			//assert.equal(router.heroesList, [1,2,3].indexOf(4));
-// 			console.log(router);
-// 		});
-// 	});
-// });
-//
+		describe('Get selected hero', function () {
+			it('get an existing hero', function () {
 
-// describe('Array', function() {
-// 	describe('#indexOf()', function() {
-// 		it('should return -1 when the value is not present', function() {
-// 			assert.equal(-1, [1,2,3].indexOf(4));
-// 		});
-// 	});
-// });
+				var currHero = heroHandler.getHeroByID(1);
+				assert.equal(currHero.name, "Moshe");
+			});
+			it('get a not existing hero', function () {
+
+				var currHero = heroHandler.getHeroByID(15);
+				assert.equal(currHero, undefined);
+			});
+		});
+	});
+
+	// Update functions test
+	describe('Update functions', function () {
+
+		it('update an exiting hero', function () {
+
+			assert.equal(heroHandler.updateHeroByID(1, "Orel"), true);
+		});
+		it('update a not exiting hero', function () {
+
+			assert.equal(heroHandler.updateHeroByID(15, "Orel"), false);
+		});
+
+	});
+
+	// Add functions test
+	describe('Add functions', function () {
+		it('add hero with not existing id', function () {
+
+			var getHeroByIDStub = {};
+			getHeroByIDStub.getHeroByID = sinon.stub();
+
+			getHeroByIDStub.getHeroByID.withArgs(11).returns(false);
+			getHeroByIDStub.getHeroByID.withArgs(1).returns(true);
+			heroHandler.getHeroByID = getHeroByIDStub.getHeroByID;
+
+			var addStatus = heroHandler.addHero(11, "Orel");
+			assert.equal(addStatus, true);
+		});
+		it('add hero with existing id', function () {
+
+			var getHeroByIDStub = {};
+			getHeroByIDStub.getHeroByID = sinon.stub();
+
+			getHeroByIDStub.getHeroByID.withArgs(11).returns(false);
+			getHeroByIDStub.getHeroByID.withArgs(1).returns(true);
+			heroHandler.getHeroByID = getHeroByIDStub.getHeroByID;
+
+			var addStatus = heroHandler.addHero(1, "Orel");
+			assert.equal(addStatus, false);
+		});
+	});
+
+	// Delete functions test
+	describe('Delete functions', function () {
+		// Delete hero by id
+		describe('Delete hero by id', function () {
+			it('delete an existing hero', function(){
+
+				var deletedStatus = heroHandler.deleteHeroByID(1);
+				assert.equal(deletedStatus, true);
+			});
+			it('delete a not existing hero', function(){
+
+				var deletedStatus = heroHandler.deleteHeroByID(15);
+				assert.equal(deletedStatus, false);
+			});
+		});
+		// Delete hero by term
+		describe('Delete hero by term', function () {
+			it('delete one existing hero', function() {
+				var deletedStatus = heroHandler.deleteHeroByTerm("J");
+				assert.equal(deletedStatus, 1);
+			});
+			it('delete more than one existing hero', function() {
+				var deletedStatus = heroHandler.deleteHeroByTerm("i");
+				assert(deletedStatus > 1);
+			});
+			it('delete a not existing hero', function() {
+				var deletedStatus = heroHandler.deleteHeroByTerm("q");
+				assert.equal(deletedStatus, 0);
+			});
+		});
+
+	});
+});
+
