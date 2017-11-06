373f769061197f27954bb3e70d015ca9f632022b
OrelZluf <orelz16@gmail.com>
2017-11-06 17:03:59 +0200

    Fixed the origin function

--- test/test.js
+++ test/test.js
@@ -4,8 +4,9 @@ var sinon = require('sinon');
 
 var heroHandler = require('../heroHandler');
 
-// Get functions test
 describe('heroHandler functions', function() {
+
+	// Get functions test
 	describe('GET functions', function () {
 		describe('Get all heroes', function () {
 			it('get all heroes', function () {
@@ -14,18 +15,19 @@ describe('heroHandler functions', function() {
 				assert.equal(heroesList.length, 10);
 			});
 		});
+	});
 
-		describe('Get selected hero', function () {
-			it('get an existing hero', function () {
+	describe('Get selected hero', function () {
+		it('get an existing hero', function () {
 
-				var currHero = heroHandler.getHeroByID(1);
-				assert.equal(currHero.name, "Moshe");
-			});
-			it('get a not existing hero', function () {
+			var currHero = heroHandler.getHeroByID(1);
+			console.log(currHero);
+			assert.equal(currHero.name, "Moshe");
+		});
+		it('get a not existing hero', function () {
 
-				var currHero = heroHandler.getHeroByID(15);
-				assert.equal(currHero, undefined);
-			});
+			var currHero = heroHandler.getHeroByID(15);
+			assert.equal(currHero, undefined);
 		});
 	});
 
@@ -50,11 +52,16 @@ describe('heroHandler functions', function() {
 			var getHeroByIDStub = {};
 			getHeroByIDStub.getHeroByID = sinon.stub();
 
-			getHeroByIDStub.getHeroByID.withArgs(11).returns(false);
-			getHeroByIDStub.getHeroByID.withArgs(1).returns(true);
+			// Save the origin function
+			var tempGetHeroByID = heroHandler.getHeroByID;
+
+			// Override the function getHeroByID
+			getHeroByIDStub.getHeroByID.withArgs(11).returns(undefined);
 			heroHandler.getHeroByID = getHeroByIDStub.getHeroByID;
 
 			var addStatus = heroHandler.addHero(11, "Orel");
+
+			heroHandler.getHeroByID = tempGetHeroByID;
 			assert.equal(addStatus, true);
 		});
 		it('add hero with existing id', function () {
@@ -62,11 +69,16 @@ describe('heroHandler functions', function() {
 			var getHeroByIDStub = {};
 			getHeroByIDStub.getHeroByID = sinon.stub();
 
-			getHeroByIDStub.getHeroByID.withArgs(11).returns(false);
-			getHeroByIDStub.getHeroByID.withArgs(1).returns(true);
+			// Save the origin function
+			var tempGetHeroByID = heroHandler.getHeroByID;
+
+			// Override the function getHeroByID
+			getHeroByIDStub.getHeroByID.withArgs(1).returns({"id" : 1, "name" : "Moshe"});
 			heroHandler.getHeroByID = getHeroByIDStub.getHeroByID;
 
 			var addStatus = heroHandler.addHero(1, "Orel");
+
+			heroHandler.getHeroByID = tempGetHeroByID;
 			assert.equal(addStatus, false);
 		});
 	});
@@ -101,7 +113,6 @@ describe('heroHandler functions', function() {
 				assert.equal(deletedStatus, 0);
 			});
 		});
-
 	});
 });
 
